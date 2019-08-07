f6b5329dac04c3b1e269451807333362bccb1f1a
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-07 19:01:31 +0900

    ~ prob02

---
+++ src/prob02/Goods.java
@@ -0,0 +1,27 @@
+package prob02;
+
+public class Goods {
+	private String goodsName;
+	private int goodsPrice;
+	private int goodsCnt;
+	
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public int getGoodsPrice() {
+		return goodsPrice;
+	}
+	public void setGoodsPrice(int goodsNum) {
+		this.goodsPrice = goodsNum;
+	}
+	public int getGoodsCnt() {
+		return goodsCnt;
+	}
+	public void setGoodsCnt(int goodsCnt) {
+		this.goodsCnt = goodsCnt;
+	}
+	
+}
--- src/prob02/GoodsApp.java
+++ src/prob02/GoodsApp.java
@@ -7,8 +7,19 @@ public class GoodsApp {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-
-		
+		Goods[] arrGoods = new Goods[COUNT_GOODS];
+		for(int i=0; i<COUNT_GOODS; i++) {			
+			Goods goods = new Goods();			
+			goods.setGoodsName(scanner.next());
+			goods.setGoodsPrice(scanner.nextInt());
+			goods.setGoodsCnt(scanner.nextInt());
+			arrGoods[i] = goods;			
+		}
+		for(int i=0; i<COUNT_GOODS;i++) {
+			System.out.println(arrGoods[i].getGoodsName()+"(가격:"+
+					arrGoods[i].getGoodsPrice()+"원)이 "+
+					arrGoods[i].getGoodsCnt()+"개 입고 되었습니다.");			
+		}
 		scanner.close();
 	}
 }
