11731b0ce7ea1bb7fb56478af235b7a283a08869
defaultuser0 <defaultuser0@DESKTOP-FDVUPSA>
2019-08-08 16:45:17 +0900

    ~ prob 06

--- src/prob03/CurrencyConverter.java
+++ src/prob03/CurrencyConverter.java
@@ -1,4 +1,15 @@
 package prob03;
 
 public class CurrencyConverter {
+	private static double rate;
+	
+	public static void setRate(double rate) {
+		CurrencyConverter.rate = rate; // static 이므로 this 가 안되니 클래스명을 붙여서 충돌 방지
+	}
+	public static double toDollar(double dollar) {
+		return dollar/rate;
+	}
+	public static double toKRW(double krw) {
+		return rate * krw;
+	}
 }
--- src/prob06/CalcApp.java
+++ src/prob06/CalcApp.java
@@ -5,7 +5,11 @@ import java.util.Scanner;
 public class CalcApp {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);		
+		Add add = new Add();
+		Sub sub = new Sub();
+		Mul mul = new Mul();
+		Div div = new Div();
 		
 		while( true ) {
 
@@ -16,24 +20,43 @@ public class CalcApp {
 				break;
 			}
 			
-			String[] tokens = expression.split( " " );
+			String[] tokens = expression.split( " ");
 			
 			if( tokens.length != 3 ) {
 				System.out.println( ">> 알 수 없는 식입니다.");
 				continue;
 			}
 			
-
+			int a = Integer.parseInt(tokens[0]);
+			int b = Integer.parseInt(tokens[2]);
 			
-			/*  코드를 완성 합니다 */
-			
-			
-			
-			
-		}
-		
+			switch(tokens[1]) {
+			case "+":
+				add.setValue(a,b);
+				System.out.println(">> "+add.calculate());				
+				printQuit();
+				return;
+			case "-":
+				sub.setValue(a,b);
+				System.out.println(">> "+sub.calculate());				
+				printQuit();
+				return;
+			case "*":
+				mul.setValue(a,b);
+				System.out.println(">> "+mul.calculate());				
+				printQuit();
+				return;
+			case "/":
+				div.setValue(a,b);
+				System.out.println(">> "+div.calculate());				
+				printQuit();
+				return;
+			}			
+		}		
 		scanner.close();
-
+	}
+	public static void printQuit() {
+		System.out.println(">> quit");
 	}
 
 }
